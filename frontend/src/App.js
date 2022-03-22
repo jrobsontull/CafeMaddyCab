acd051ce53d4fb4de3662d090473481dfadc3743
galacticfan <jakerobsontull@gmail.com>
2022-03-21 20:15:51 -0400

    Landing page work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+
+import './assets/css/global.css';
+import LandingPage from './components/LandingPage';
+import RequestRide from './components/RequestRide';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <BrowserRouter>
+        <Routes>
+          <Route path={'/'} element={<LandingPage />} />
+          <Route path={'/request-ride'} element={<RequestRide />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/components/RequestRide.js
@@ -0,0 +1,14 @@
+function RequestRide() {
+  return (
+    <div className="react-container">
+      <div className="content">
+        <div className="titles">
+          <h2>CAFE MADDY CAB</h2>
+          <h3>Ride Reimbursment</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default RequestRide;
