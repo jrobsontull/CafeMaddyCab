fec7fb1800985cdb2282f0e2180f772dd18ba8a4
galacticfan <jakerobsontull@gmail.com>
2022-04-20 17:46:31 -0400

    fix: Try chaning URL for requests to http

--- frontend/src/utils/http.common.js
+++ frontend/src/utils/http.common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 let baseU = 'http://localhost:8080/';
 if (process.env.NODE_ENV === 'production') {
-  baseU = 'https://cafemaddycab.org:8080/';
+  baseU = 'http://cafemaddycab.org:8080/';
   console.log('Base API URL set to PROD.');
 } else {
   console.log('Base API URL set to DEV.');
