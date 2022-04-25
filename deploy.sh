b54d7444d64aa20abc4a1f32979f896a4bc66124
galacticfan <jakerobsontull@gmail.com>
2022-04-25 19:06:07 -0400

    fix: alterations to bash script

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ git pull
 
 read -p "Do you need to install node.js modules (y/n)? " runInstall
 
-if [$runInstall == "y"]; then
+if [$runInstall -eq "y"]; then
 echo "Installing frontend modules"
 cd frontend/
 npm install
